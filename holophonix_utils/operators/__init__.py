5a4b33c8ca64efc48f8e3a2db10cf9de6b3478b5
dewiweb <dewiweb@gmail.com>
2025-03-11 22:10:14 +0100

    Initialize new feature structure for project import and venue loading

--- holophonix_utils/operators/__init__.py
+++ holophonix_utils/operators/__init__.py
@@ -4,8 +4,12 @@ from .import_an_tree import SNA_OT_Import_An_Tree_433Db
 from .sources_exporter import SNA_OT_Sources_Exporter_34F69
 from .add_speakers import SNA_OT_Add_Speakers_994C8
 from .add_handlers import SNA_OT_Add_Handlers
+from .import_holophonix_project import SNA_OT_Import_Holophonix_Project
+from .load_venue import SNA_OT_Load_Venue
 
 __all__ = [
+    "SNA_OT_Load_Venue",
+    "SNA_OT_Import_Holophonix_Project",
     "SNA_OT_Delete_Handlers_C2D71",
     "SNA_OT_Add_Sources_73B0D",
     "SNA_OT_Import_An_Tree_433Db",
