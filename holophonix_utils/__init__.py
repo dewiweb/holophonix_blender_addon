2070fac247cf48a942525759eaf1076afdd63f35
dewiweb <dewiweb@gmail.com>
2025-03-14 09:17:13 +0100

    fix: import HolophonixUtilsProperties directly instead of using wildcard

--- holophonix_utils/__init__.py
+++ holophonix_utils/__init__.py
@@ -16,7 +16,7 @@ bl_info = {
     "author" : "Dewiweb",
     "description" : "",
     "blender" : (4, 3, 0),
-    "version" : (1, 0, 6),  # Updated version to trigger the release workflow
+    "version" : (1, 1, 0),  # Updated version to trigger the release workflow
     "location" : "",
     "warning" : "",
     "doc_url": "",
@@ -26,7 +26,7 @@ bl_info = {
 
 import bpy
 import os
-from .utils import *
+from .utils import HolophonixUtilsProperties
 from .panels import *
 from .operators import *
 
