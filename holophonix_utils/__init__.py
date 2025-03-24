73931251b38649cb513d31f51f3b0f930d19e2f9
dewiweb <dewiweb@gmail.com>
2025-03-24 22:49:38 +0100

    Fix addon registration issues

--- holophonix_utils/__init__.py
+++ holophonix_utils/__init__.py
@@ -79,6 +79,10 @@ def register():
     from bpy.utils import register_class, previews
     import os
 
+    # Register AttributeValue first
+    from .utils.track_handler_proxy import AttributeValue
+    register_class(AttributeValue)
+
     # Load custom icon
     icon_path = os.path.join(os.path.dirname(__file__), 'icons', 'logo_icon.png')
     custom_icons = previews.new()
@@ -87,7 +91,8 @@ def register():
 
     # Register all classes
     for cls in classes:
-        bpy.utils.register_class(cls)
+        if cls != AttributeValue:  # Skip AttributeValue since we already registered it
+            bpy.utils.register_class(cls)
 
     # Add custom property to the scene
     bpy.types.Scene.holophonix_utils = bpy.props.PointerProperty(type=HolophonixUtilsProperties)
